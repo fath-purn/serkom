df2ef3bc5356584990f962e764abaf2a1d992f8b
Fatkhurrohaman Purnomo <talitakamilia345@gmail.com>
2024-03-15 09:41:09 +0700

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Customers() {
+  return (
+    <div>
+      <h1>Customers</h1>
+    </div>
+  );
+}
